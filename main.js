46d318eeb1d202dd9e8a81ec9335bf1df8b36962
dafidka <55770908+dafidka@users.noreply.github.com>
2021-04-05 18:21:15 +0200

    stuff

---
+++ main.js
@@ -0,0 +1,60 @@
+const Discord = require("discord.js");
+const fs = require('fs');
+
+const client = new Discord.Client();
+const myID = 828673255173783612;
+
+var b_valasz = false;
+var token = fs.readFileSync('token', 'utf8');
+
+function random(min, max) {
+    return Math.floor(
+        Math.random() * (max - min) + min
+    )
+}
+
+
+client.on('message', gotMessage);
+function gotMessage(msg) {
+    //console.log(msg.content);
+    //console.log(msg.author.id);
+
+    if (msg.content.includes !== '$' && b_valasz && msg.author.id != myID) {
+
+        switch (random(0,4)) {
+            case 1: msg.channel.send('kerdeztuk 🤡');
+                break;
+            case 2: msg.channel.send('fogd mar be 🤐');
+                break;
+            case 3: msg.channel.send('kurva anyad 🔫');
+                break;
+        }
+    }
+
+    if (msg.content.includes('$')) {
+        //msg.channel.send('kerdeztuk');
+        
+        if (msg.content.includes('valasz') || msg.content == '$v') {
+            b_valasz = !b_valasz;
+            switch (b_valasz) {
+                case true: msg.channel.send('valasz bekapcsolva');
+                    break;
+                case false: msg.channel.send('valasz kikapcsolva');
+                    break;
+                
+            }
+        }
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+client.login(token)
